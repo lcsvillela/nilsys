0371d453ad03878fab8a120a9fc0cca544eda10c
lcsvillela <23728459+lcsvillela@users.noreply.github.com>
2022-02-08 03:23:25 -0300

    correcoes no sistema de login e adicão de mais botões

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
-    helper_method :logged_in?
-    helper_method :current_user
+    helper_method :current_user, :logged_in?
 
     def current_user
         User.find_by(id: session[:user_id])
     end
 
     def logged_in?
-        !current_user.nil?
+      session[:user_id]
     end
 end
