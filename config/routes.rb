474afe49f474327ae7fa7399fdef13d95e8e9674
lcsvillela <23728459+lcsvillela@users.noreply.github.com>
2022-02-07 23:14:31 -0300

    Nil System

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  get 'sessions/principal'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  #
+  root to: 'sessions#principal'
+  resources :users
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
+end
